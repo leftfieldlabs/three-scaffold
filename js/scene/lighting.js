360c966aeb72543f8aefef6f36f5837c4fc74893
Tom Slezakowski <tom@leftfieldlabs.com>
2016-10-26 19:40:05 -0700

    Reverting to ES5

--- js/scene/lighting.js
+++ js/scene/lighting.js
@@ -1,22 +1,13 @@
-import { Object3D, AmbientLight } from 'three';
+var THREE = require('Three');
 
+var Lighting = function() {
+  THREE.Object3D.call(this);
 
-/**
- * Gives our scene some light. Light helps us see things.
- * @constructor
- **/
-class Lighting extends Object3D {
-  constructor(props) {
-    super(props);
+  this.ambLight = new THREE.AmbientLight(0xffffff, 0.8);
+  this.add(this.ambLight);
+};
 
-    this.ambLight = new AmbientLight(0xffffff, 0.8);
-    this.add(this.ambLight);
+Lighting.constructor = Lighting;
+Lighting.prototype = Object.create(THREE.Object3D.prototype);
 
-    // this.topLight = new THREE.DirectionalLight(0xffffff, 1.7);
-    // this.topLight.position.set(10, 10, 10);
-    // this.add(this.topLight);
-
-  }
-}
-
-export default Lighting;
+module.exports = Lighting;
--- readme.md
+++ readme.md
@@ -1,2 +1,24 @@
-# three.js scaffold
-Scaffold for three.js projects
+![N|Solid](http://www.leftfieldlabs.com/assets/images/logo-main-inverted.png)
+
+# Interactive globe showing custom data-sets
+---
+### Some description will go here
+ ---   
+### Installation
+  1. Use the Node Package Manager to install additional libraries and utilities.
+```sh
+$ npm install
+```
+
+### Building for deployment
+  1. Run the build command.
+```sh
+$ gulp build
+```
+  > After a few minutes, this will complete and your final package of files will be found in `/build/` - You can copy the contents of that folder to any server, point your browser to the `index.html` and hey presto!
+
+### Development Env
+  1. Run the sync command.
+```sh
+$ gulp serve
+``` 
